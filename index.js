3a0e7c06f9f55d1763090e8b090aa0c314ccf6eb
Rohit Rana <rkrana6631@gmail.com>
2023-08-21 10:33:16 +0530

    ok

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.use(express.json());
 const userroute = require('./routes/user_route');
 const quizroute = require('./routes/quiz_route');
 
-app.use("/api",userroute);
-app.use("/api",quizroute);
+
 
 mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/quiz_dashboard?retryWrites=true&w=majority',
 {useUnifiedTopology: true,useNewUrlParser: true},
 ).then(() => app.listen(3000)
 ).then(() => console.log("connected to Database and running on port 3000")
 );
+
+app.use("/api",userroute);
+app.use("/api",quizroute);
