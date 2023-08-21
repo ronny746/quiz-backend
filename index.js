97f5c61d67c3266479e0f7a602b09a23e4e322a4
Rohit Rana <rkrana6631@gmail.com>
2023-08-21 11:00:39 +0530

    done

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ dotenv.config();
 var express = require('express');
 const app = express();
 app.use(express.json());
-const userroute = require('./routes/user_route');
-const quizroute = require('./routes/quiz_route');
+// const userroute = require('./routes/user_route');
+// const quizroute = require('./routes/quiz_route');
 
-
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+  })
 
 mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/quiz_dashboard?retryWrites=true&w=majority',
 {useUnifiedTopology: true,useNewUrlParser: true},
@@ -15,7 +17,7 @@ mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/quiz_das
 ).then(() => console.log("connected to Database and running on port 3000")
 );
 
-app.use("/api",userroute);
-app.use("/api",quizroute);
+// app.use("/api",userroute);
+// app.use("/api",quizroute);
 
 module.exports = app
